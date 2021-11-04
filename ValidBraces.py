56d4390f582c86f2b5ed052d1ed9535aea13431f
SuperFunkyNinja <68922959+SuperFunkyNinja@users.noreply.github.com>
2021-11-04 13:17:14 +0000

    commit

--- ValidBraces.py
+++ ValidBraces.py
@@ -1,5 +1,20 @@
+import re
+
+
 def validBraces(string):
-    return
+    pattern = re.compile(r"\(\)|\[\]|\{\}")
+    gp = pattern.search(string)
+    if gp is None:
+        return "False"
+    string = str.replace(string, gp.group(0), "")
+    print(string)
+    print(gp)
+    if len(string) == 0:
+        return True
+    elif gp is not None:
+        return False
+    else:
+        validBraces(string)
 
 
-validBraces("[({})](]")
+print(validBraces("[({})]()[]"))
