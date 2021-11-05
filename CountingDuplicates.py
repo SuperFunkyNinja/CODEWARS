60dff6c3738f522069072fd0a9a7f71f8d13385e
SuperFunkyNinja <68922959+SuperFunkyNinja@users.noreply.github.com>
2021-11-05 13:10:38 +0000

    countingduplciates finished

---
+++ CountingDuplicates.py
@@ -0,0 +1,38 @@
+def duplicate_count(text):
+    # newlist = []
+    # characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    # text = text.lower()
+    # print(len(["i" for i in characters if text.count(i) > 1]))
+
+    return len(
+        [
+            "i"
+            for i in "abcdefghijklmnopqrstuvwxyz0123456789"
+            if text.lower().count(i) > 1
+        ]
+    )
+
+    # for i in "abcdefghijklmnopqrstuvwxyz":
+    #     if text.count(i) > 1:
+    #         newlist.append(i)
+
+    # for i in text:
+    #     newlist.append(text.count(i))
+
+    # for i, j in enumerate(text):
+    #     if j in text[i - 1 : i + 1]:
+    #         newlist.append(j)
+
+    # newlist = list(dict.fromkeys(newlist))
+    # print(text)
+    # print(newlist)
+    # print(len(newlist))
+
+
+duplicate_count("abcde")  # 0 no characters repeats more than once
+duplicate_count("aabbcde")  # 2 'a' and 'b'
+duplicate_count("aabBcde")  # 2 'a' occurs twice and 'b' twice (`b` and `B`)
+duplicate_count("indivisibility")  # 1 'i' occurs six times
+duplicate_count("Indivisibilities")  # 2 'i' occurs seven times and 's' occurs twice
+duplicate_count("aA11")  # 2 'a' and '1'
+duplicate_count("ABBA")  # 2 'A' and 'B' each occur twice
